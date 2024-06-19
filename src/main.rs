4117dce70601e510edac9874ab204efd502ad6b0
Laurent Wouters <lwouters@cenotelie.fr>
2024-06-19 10:48:46 +0200

    Step 1: Write the input code sample into a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 #![forbid(unsafe_code)]
 #![warn(clippy::pedantic)]
 
+use tokio::io::AsyncWriteExt;
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+
+    Ok(())
+}
+
+const SOURCE_FILE_NAME: &str = "main.rs";
+
+/// Compiles the sample
+async fn compile_input(sample: &str) -> Result<(), anyhow::Error> {
+    {
+        let source_file = tokio::fs::File::create(SOURCE_FILE_NAME).await?;
+        let mut writer = tokio::io::BufWriter::new(source_file);
+        writer.write_all(sample.as_bytes()).await?;
+        writer
+            .write_all(b"\n\n#[no_mangle]\npub extern \"C\" fn __entry() { let _ = main(); }\n")
+            .await?;
+        writer.flush().await?;
+    }
 
     Ok(())
 }
